c600a37da7346a004c84395f7bff11b9ac01317d
Silvestre-CarrilloRamirez <silvestrecarrillo05@gmail.com>
2025-07-28 23:04:29 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int edad;
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+    
+    if (edad >=18) {
+        cout << "Usted es mayor de edad." << endl;
+        } else {
+                cout << "Usted es menor de edad." <<endl;
+        }
+        return 0;
+}
